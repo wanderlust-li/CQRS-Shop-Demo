e56768a4a61ea7df230c28df93e37bf2dfdf72a0
wanderlust-li <zhekalichm4n@icloud.com>
2023-09-03 12:30:19 +0300

    add product handler

---
+++ ShopDemo.Application/Commands/Product/CreateProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace ShopDemo.Application.Commands.Product;
+
+public class CreateProductCommand : IRequest<object>
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public decimal Price { get; set; }
+    
+    public int Quantity { get; set; }
+}
---
+++ ShopDemo.Application/Handlers/Product/CreateProductCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using ShopDemo.Application.Commands.Product;
+using ShopDemo.Application.Repository.IRepository;
+
+namespace ShopDemo.Application.Handlers.Product;
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, object>
+{
+    private readonly IProductRepository _db;
+
+    public CreateProductCommandHandler(IProductRepository db)
+    {
+        _db = db;
+    }
+
+    public async Task<object> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Domain.Entities.Product()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Price = request.Price,
+            Quantity = request.Quantity
+        };
+
+        try
+        {
+            await _db.CreateAsync(product);
+
+            return new { message = "Succesfuly created product" };
+        }
+        catch (Exception ex)
+        {
+            return new { error = ex.Message };
+        }
+    }
+}
