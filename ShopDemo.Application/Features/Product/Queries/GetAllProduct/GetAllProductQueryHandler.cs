e7f92188c7dc1d7cc37d9a8f6fd1931b83853344
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-12 15:08:23 +0300

    update getAllProductHandler

--- ShopDemo.Application/Features/Product/Queries/GetAllProduct/GetAllProductQueryHandler.cs
+++ ShopDemo.Application/Features/Product/Queries/GetAllProduct/GetAllProductQueryHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace ShopDemo.Application.Features.Product.Queries.GetAllProduct;
+﻿using AutoMapper;
+using MediatR;
+using ShopDemo.Application.Contracts.ProductRepository;
 
-public class GetAllProductQueryHandler
+namespace ShopDemo.Application.Features.Product.Queries.GetAllProduct;
+
+public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, List<ProductDto>>
 {
-    
+    private readonly IMapper _mapper;
+    private readonly IProductRepository _productRepository;
+
+    public GetAllProductQueryHandler(IMapper mapper, IProductRepository productRepository)
+    {
+        this._mapper = mapper;
+        this._productRepository = productRepository;
+    }
+
+    public async Task<List<ProductDto>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _productRepository.GetAsync();
+
+        var data = _mapper.Map<List<ProductDto>>(products);
+
+        return data;
+    }
 }
--- ShopDemo.Application/MappingProfiles/ProductProfile.cs
+++ ShopDemo.Application/MappingProfiles/ProductProfile.cs
@@ -1,6 +1,13 @@
-﻿namespace ShopDemo.Application.MappingProfiles;
+﻿using AutoMapper;
+using ShopDemo.Application.Features.Product.Queries.GetAllProduct;
+using ShopDemo.Domain;
 
-public class ProductProfile
+namespace ShopDemo.Application.MappingProfiles;
+
+public class ProductProfile : Profile
 {
-    
+    public ProductProfile()
+    {
+        CreateMap<ProductDto, Product>().ReverseMap();
+    }
 }
