ee2d3d53366cabaabb39698dae33ee7fd508bfe2
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-16 18:44:37 +0300

    add delete product command

--- ShopDemo.Application/Features/Product/Commands/DeleteProduct/DeleteProductCommand.cs
+++ ShopDemo.Application/Features/Product/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -1,6 +1,8 @@
-﻿namespace ShopDemo.Application.Features.Product.Commands.DeleteProduct;
+﻿using MediatR;
 
-public class DeleteProductCommand
+namespace ShopDemo.Application.Features.Product.Commands.DeleteProduct;
+
+public class DeleteProductCommand: IRequest<Unit>
 {
-    
+    public int Id { get; set; }
 }
---
+++ ShopDemo.Application/Features/Product/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using ShopDemo.Application.Contracts.ProductRepository;
+
+namespace ShopDemo.Application.Features.Product.Commands.DeleteProduct;
+
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Unit>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.Id);
+
+        await _productRepository.DeleteAsync(product);
+
+        return Unit.Value;
+    }
+}
