afb669a2bfb72ed030f96792dd21bb4dcce7fb52
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-18 00:24:57 +0300

    update IdentityServicesRegistration

--- ShopDemo.Identity/IdentityServicesRegistration.cs
+++ ShopDemo.Identity/IdentityServicesRegistration.cs
@@ -1,5 +1,15 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using ShopDemo.Application.Contracts.Identity;
+using ShopDemo.Application.Models.Identity;
+using ShopDemo.Identity.DbContext;
+using ShopDemo.Identity.Models;
+using ShopDemo.Identity.Services;
 
 namespace ShopDemo.Identity;
 
@@ -7,6 +17,37 @@ public static class IdentityServicesRegistration
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        
+        services.AddDbContext<ShopDemoIdentityDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ShopDatabaseConnectionString")));
+        
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ShopDemoIdentityDbContext>().AddDefaultTokenProviders();
+        
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddTransient<IUserService, UserService>();
+        
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(o =>
+        {
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = configuration["JwtSettings:Issuer"],
+                ValidAudience = configuration["JwtSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+
+            };
+        });
+        
         return services;
     }
 }
