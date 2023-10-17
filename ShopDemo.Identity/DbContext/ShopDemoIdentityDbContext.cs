aa6fa810c33d083aad0a80369641c74c3c3d293d
wanderlust-li <zhekalichm4n@icloud.com>
2023-10-18 00:22:09 +0300

    add Configurations in identity

---
+++ ShopDemo.Identity/DbContext/ShopDemoIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopDemo.Identity.Models;
+
+namespace ShopDemo.Identity.DbContext;
+
+public class ShopDemoIdentityDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ShopDemoIdentityDbContext(DbContextOptions<ShopDemoIdentityDbContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ShopDemoIdentityDbContext).Assembly);
+    }
+}
